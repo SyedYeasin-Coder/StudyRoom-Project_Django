bc91f6416966de6e37bf83d3c955f6e2c2e0d310
Syed <finexgamer4@gmail.com>
2025-03-21 21:06:46 +0600

    Some Minor CSS Changes

--- base/signals.py
+++ base/signals.py
@@ -2,15 +2,14 @@ from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
 from .models import Message, Room
 
-# Update Room.updated when a new message is sent or an existing message is deleted
 @receiver([post_save, post_delete], sender=Message)
 def update_room_on_message_change(sender, instance, **kwargs):
     instance.room.updated = instance.updated
     instance.room.save()
 
-# Update Room.updated when a participant is added or removed
+
 @receiver(m2m_changed, sender=Room.participants.through)
 def update_room_on_participant_change(sender, instance, action, **kwargs):
     if action in ["post_add", "post_remove"]:
-        instance.updated = instance.updated  # This triggers auto_now=True
+        instance.updated = instance.updated  
         instance.save()
